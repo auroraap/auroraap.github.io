f78d1ce587427f5fd268e778f7d8d08282a8e573
Aurora <78878792+auroraap@users.noreply.github.com>
2022-06-16 09:11:59 +0200

    Initial commit. displayLeaderboard og saveGame funker ikke.

---
+++ script.js
@@ -0,0 +1,72 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+let Players = [];
+
+//create a function to make a random time for mole to pop from the hole
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+    const index  = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+
+    //prevent same hole from getting the same number
+    if (hole === lastHole){
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+
+function peep() {
+    const time = randomTime(500, 1000); //get a random time to determine how long mole should peep
+    const hole = randomHole(holes); //get the random hole from the randomHole function
+    hole.classList.add('up'); //add the CSS class so selected mole can "pop up"
+    setTimeout(() => {
+        hole.classList.remove('up'); //make the selected mole "pop down" after a random time
+        if(!timeUp) {
+            peep();
+        }
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 15000) //show random moles for 15 seconds
+}
+
+function displayLeaderboard() {
+    let theExport = ""; 
+    Players.sort((aPlayer, bPlayer) => aPlayer.score - bPlayer.score);
+    Players.forEach((player) => theExport += '<tr><td>' + player.name + '</td><td>' + player.score + '</td></tr>');
+    document.getElementById("thingy").innerHTML = theExport; //Why have good ID's when you can have bad ones? | Who needs children when we can use innerHTML?
+}
+
+displayLeaderboard();
+
+function wack(e){
+    if(!e.isTrusted) return; //** new thing I learned */
+    score++;
+    this.parentNode.classList.remove('up'); //this refers to item clicked
+    scoreBoard.textContent = score;
+}
+
+moles.forEach(mole => mole.addEventListener('click', wack))
+
+function Player(myName, myScore) {
+    this.name = myName;
+    this.score = myScore;
+}
+
+function saveGame(player) {
+        p1 = new Player(player, score);
+        Players.add(p1);
+}
